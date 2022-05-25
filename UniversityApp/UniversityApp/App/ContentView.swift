b89412fc263acdcc0838975a7bb801889e354e46
昊 李 <18888911937@163.com>
2022-05-25 22:09:19 +0800

    添加资源和视图

--- UniversityApp/UniversityApp/App/ContentView.swift
+++ UniversityApp/UniversityApp/App/ContentView.swift
@@ -1,88 +1,110 @@
 //
 //  ContentView.swift
-//  UniversityApp
+//  ThePlacesOfInterest
 //
-//  Created by admin on 2022/5/5.
+//  Created by 刘铭 on 2021/1/3.
 //
 
 import SwiftUI
-import CoreData
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) private var viewContext
-
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
-        animation: .default)
-    private var items: FetchedResults<Item>
-
-    var body: some View {
-        NavigationView {
-            List {
-                ForEach(items) { item in
-                    NavigationLink {
-                        Text("Item at \(item.timestamp!, formatter: itemFormatter)")
-                    } label: {
-                        Text(item.timestamp!, formatter: itemFormatter)
-                    }
-                }
-                .onDelete(perform: deleteItems)
-            }
-            .toolbar {
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    EditButton()
-                }
-                ToolbarItem {
-                    Button(action: addItem) {
-                        Label("Add Item", systemImage: "plus")
-                    }
-                }
-            }
-            Text("Select an item")
-        }
+  //MARK: - Properties
+  let places: [Place] = Bundle.main.decode("places.json")
+  let haptics = UIImpactFeedbackGenerator(style: .medium)
+  
+  @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+  @State private var gridColumn = 1
+  @State private var toolbarIcon = "square.grid.2x2"
+  
+  @State private var isGridViewActive: Bool = false
+  
+  // MARK: - Function
+  func gridSwitch() {
+    gridLayout = Array(repeating: GridItem(.flexible()), count: gridLayout.count % 3 + 1)
+    gridColumn = gridLayout.count
+    
+    // 按钮外观变换
+    switch gridColumn {
+    case 1:
+      toolbarIcon = "square.grid.2x2"
+    case 2:
+      toolbarIcon = "square.grid.3x2"
+    case 3:
+      toolbarIcon = "rectangle.grid.1x2"
+    default:
+      toolbarIcon = "square.grid.2x2"
     }
-
-    private func addItem() {
-        withAnimation {
-            let newItem = Item(context: viewContext)
-            newItem.timestamp = Date()
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
-
-    private func deleteItems(offsets: IndexSet) {
-        withAnimation {
-            offsets.map { items[$0] }.forEach(viewContext.delete)
-
-            do {
-                try viewContext.save()
-            } catch {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-                let nsError = error as NSError
-                fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
-            }
-        }
-    }
+  }
+  
+  //MARK: - Body
+  var body: some View {
+    NavigationView {
+      Group {
+        if !isGridViewActive {
+          List {
+            CoverImageView()
+              .frame(height: 300)
+              .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            
+            ForEach(places) { place in
+              NavigationLink(destination: PlaceDetailView(place: place)) {
+                PlaceListItemView(place: place)
+              } //: Link
+            } //: Loop
+            CopyrightView()
+              .modifier(CenterModifier())
+          } //: List
+        } else {
+          ScrollView(.vertical, showsIndicators: false) {
+            LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+              ForEach(places) { item in
+                NavigationLink(
+                  destination: PlaceDetailView(place: item)) {
+                    PlaceGridItemView(place: item)
+                  } //: Link
+              } //: Loop
+            } //: LazyVGrid
+            .padding(10)
+            .animation(.easeIn)
+          } //: ScrollView
+        } //: Endif
+      } //: Group
+      .navigationBarTitle("名胜古迹", displayMode: .large)
+      .toolbar {
+        ToolbarItem(placement: .navigationBarTrailing) {
+          HStack(spacing: 16) {
+            // 列表视图模式
+            Button(action: {
+              print("列表视图被激活")
+              isGridViewActive = false
+              haptics.impactOccurred()
+            }){
+              Image(systemName: "square.fill.text.grid.1x2")
+                .font(.title2)
+                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+            } //: List Button
+            
+            // 网格视图模式
+            Button(action: {
+              print("网格视图被激活")
+              isGridViewActive = true
+              haptics.impactOccurred()
+              gridSwitch()
+            }){
+              Image(systemName: toolbarIcon)
+                .font(.title2)
+                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+            } //: Grid Button
+          } //: HStack
+        } //: ToolBarItem
+      } //: Toolbar
+    } //: NavigationView
+  }
 }
 
-private let itemFormatter: DateFormatter = {
-    let formatter = DateFormatter()
-    formatter.dateStyle = .short
-    formatter.timeStyle = .medium
-    return formatter
-}()
-
+//MARK: - Preview
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environment(\.managedObjectContext, PersistenceController.preview.container.viewContext)
-    }
+  static var previews: some View {
+    ContentView()
+  }
 }
